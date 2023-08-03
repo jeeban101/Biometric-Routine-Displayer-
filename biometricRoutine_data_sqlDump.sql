f7ea578be4161d5f5210f16d1b4a11ef625fcb4b
jeeban101 <91117991+jeeban101@users.noreply.github.com>
2023-08-03 18:40:10 +0545

    Add files via upload

---
+++ biometricRoutine_data_sqlDump.sql
@@ -0,0 +1,113 @@
+CREATE TABLE monday (
+    id TEXT(9),
+    name TEXT(30),
+    first TEXT(30),
+    second TEXT(30),
+    third TEXT(30),
+    fourth TEXT(30),
+    fifth TEXT(30),
+    sixth TEXT(30),
+    seventh TEXT(30),
+    eighth TEXT(30),
+    ninth TEXT(30),
+    tenth TEXT(30),
+    eleventh TEXT(30),
+    extra TEXT(30)
+);
+CREATE TABLE tuesday (
+    id TEXT(9),
+    name TEXT(30),
+    first TEXT(30),
+    second TEXT(30),
+    third TEXT(30),
+    fourth TEXT(30),
+    fifth TEXT(30),
+    sixth TEXT(30),
+    seventh TEXT(30),
+    eighth TEXT(30),
+    ninth TEXT(30),
+    tenth TEXT(30),
+    eleventh TEXT(30),
+    extra TEXT(30)
+);
+CREATE TABLE wednesday (
+    id TEXT(9),
+    name TEXT(30),
+    first TEXT(30),
+    second TEXT(30),
+    third TEXT(30),
+    fourth TEXT(30),
+    fifth TEXT(30),
+    sixth TEXT(30),
+    seventh TEXT(30),
+    eighth TEXT(30),
+    ninth TEXT(30),
+    tenth TEXT(30),
+    eleventh TEXT(30),
+    extra TEXT(30)
+);
+CREATE TABLE thursday (
+    id TEXT(9),
+    name TEXT(30),
+    first TEXT(30),
+    second TEXT(30),
+    third TEXT(30),
+    fourth TEXT(30),
+    fifth TEXT(30),
+    sixth TEXT(30),
+    seventh TEXT(30),
+    eighth TEXT(30),
+    ninth TEXT(30),
+    tenth TEXT(30),
+    eleventh TEXT(30),
+    extra TEXT(30)
+);
+CREATE TABLE friday (
+    id TEXT(9),
+    name TEXT(30),
+    first TEXT(30),
+    second TEXT(30),
+    third TEXT(30),
+    fourth TEXT(30),
+    fifth TEXT(30),
+    sixth TEXT(30),
+    seventh TEXT(30),
+    eighth TEXT(30),
+    ninth TEXT(30),
+    tenth TEXT(30),
+    eleventh TEXT(30),
+    extra TEXT(30)
+);
+
+
+INSERT INTO monday VALUES (
+    "20BIT0440", "Jeeban",
+    "Chemistry", "Environment Science", "Greek", "Free period", "Free period", "LUNCH BREAK", 
+    "Java programming", "Discrete Mathematics", "Free period", "Soft skills", "Calculus", 
+    "Physics Seminar - online "
+);
+INSERT INTO tuesday VALUES (
+    "20BIT1234", "Alice",
+    "English", "Mathematics", "History", "LUNCH BREAK", "Physics", "Geography", 
+    "Chemistry", "Biology", "Free period", "Social Studies", "Computer Science", 
+    "Sports Activities"
+);
+INSERT INTO wednesday VALUES (
+    "20BIT5678", "Bob",
+    "Economics", "Business Studies", "Accounting", "LUNCH BREAK", "Statistics", 
+    "English Literature", "Geography", "Political Science", "Free period", "Mathematics", 
+    "Physical Education", "Cultural Event"
+);
+INSERT INTO thursday VALUES (
+    "20BIT2468", "Carol",
+    "Physics", "Chemistry", "Biology", "LUNCH BREAK", "Mathematics", "English", 
+    "Computer Science", "History", "Free period", "Environmental Science", 
+    "Geography", "Debate Club Meeting"
+);
+
+INSERT INTO friday VALUES (
+    "20BIT1357", "David",
+    "Computer Science", "Mathematics", "Physics", "LUNCH BREAK", "Chemistry", "Biology", 
+    "English", "Geography", "Free period", "Social Studies", "Physical Education", 
+    "Art and Craft"
+);
